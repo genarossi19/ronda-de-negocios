5de0b891b7fc92646c1c393eb49719e6f60b80c6
genarossi19 <genaros.gr@gmail.com>
2025-10-07 12:25:01 -0300

    agregando config de ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(() => {
+  // Leemos la variable desde el entorno (.env.development, .env, etc.)
+  const allowNgrok = process.env.VITE_ALLOW_NGROK === "true";
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      host: true, // Permite acceder desde LAN o túneles externos (ngrok)
+      allowedHosts: allowNgrok ? [".ngrok-free.app"] : [], // Permitir ngrok solo si está habilitado
+    },
+  };
 });
